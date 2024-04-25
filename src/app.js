93c1ebf9bb9bbc00981a904908b1de193b9bcced
Nach0rr0 <ignacio.roman@live.cl>
2024-04-25 14:51:27 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,73 @@
+import MongoStore from 'connect-mongo';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import handlebars from 'express-handlebars';
+import session from 'express-session';
+import passport from 'passport';
+import path from 'path';
+import { iniPassport } from './config/passport.config.js';
+import { authRouter } from './routes/auth.router.js';
+import { cartsRouter } from './routes/carts.router.js';
+import { chatRouter } from './routes/chats.router.js';
+import { coockieRouter } from './routes/cookies.router.js';
+import { cartsHtml } from './routes/homeCarts.router.js';
+import { productsHtml } from './routes/homeProducts.router.js';
+import { productsRouter } from './routes/products.router.js';
+import { productsRealTime } from './routes/realTimeProducts.router.js';
+import { sessionsRouter } from './routes/sessions.router.js';
+import { __dirname, connectMongo, connectSocket } from './utils.js';
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const httpServer = app.listen(port, () => {
+  console.log(`🍕 App listening on port ➡️  http://localhost:${port}`);
+});
+
+connectMongo();
+
+app.use(cookieParser());
+
+app.use(
+  session({
+    store: MongoStore.create({ mongoUrl: 'mongodb+srv://nachuroman23:<password>@cluster0.awelxst.mongodb.net/', ttl: 7200 }),
+    secret: 'un-re-secreto',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+iniPassport();
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/products', productsRouter);
+app.use('/api/carts', cartsRouter);
+
+app.get('/', (req, res) => {
+  return res.redirect('http://localhost:8080/auth/login');
+});
+app.use('/products', productsHtml);
+app.use('/carts', cartsHtml);
+app.use('/realtimeproducts', productsRealTime);
+app.use('/chat', chatRouter);
+app.use('/auth', authRouter);
+
+app.use('/api/cookies', coockieRouter);
+
+app.use('/api/sessions', sessionsRouter);
+
+app.engine('handlebars', handlebars.engine());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'handlebars');
+
+connectSocket(httpServer);
+
+app.get('*', (req, res) => {
+  return res.status(404).json({ status: 'error', message: 'No encontrado' });
+});
---
+++ src/config/passport.config.js
@@ -0,0 +1,136 @@
+import fetch from 'node-fetch';
+import passport from 'passport';
+import GitHubStrategy from 'passport-github2';
+import local from 'passport-local';
+import { UserModel } from '../DAO/models/users.model.js';
+import { createHash, isValidPassword } from '../utils.js';
+import { CartService } from '../services/carts.service.js';
+
+const LocalStrategy = local.Strategy;
+const cartService = new CartService();
+
+export function iniPassport() {
+  passport.use(
+    'login',
+    new LocalStrategy({ usernameField: 'email' }, async (username, password, done) => {
+      try {
+        const user = await UserModel.findOne({ email: username });
+        if (!user) {
+          console.log('Usuario no encontrado con nombre de usuario (email) ' + username);
+          return done(null, false);
+        }
+        if (!isValidPassword(password, user.password)) {
+          console.log('Contraseña invalida');
+          return done(null, false);
+        }
+
+        if (user.cart) {
+          console.log(user.cart);
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    })
+  );
+
+  passport.use(
+    'Registro',
+    new LocalStrategy(
+      {
+        passReqToCallback: true,
+        usernameField: 'email',
+      },
+      async (req, username, password, done) => {
+        try {
+          const { email, firstName, lastName, age } = req.body;
+          let user = await UserModel.findOne({ email: username });
+          if (user) {
+            console.log('Usuario ya existe');
+            return done(null, false);
+          }
+
+          const newUser = {
+            email,
+            firstName,
+            lastName,
+            age: Number(age),
+            isAdmin: false,
+            password: createHash(password),
+            cart: await cartService.createCart(),
+          };
+
+          let userCreated = await UserModel.create(newUser);
+          console.log(userCreated);
+          console.log('Registro de usuario exitoso');
+          return done(null, userCreated);
+        } catch (e) {
+          console.log('Error en el registro');
+          console.log(e);
+          return done(e);
+        }
+      }
+    )
+  );
+
+  passport.use(
+    'github',
+    new GitHubStrategy(
+      {
+        clientID: '',
+        clientSecret: '',
+        callbackURL: 'http://localhost:8080/api/sessions/githubcallback',
+      },
+      async (accesToken, _, profile, done) => {
+        try {
+          const res = await fetch('https://api.github.com/user/emails', {
+            headers: {
+              Accept: 'application/vnd.github.v3+json',
+              Authorization: 'Bearer ' + accessToken,
+            },
+          });
+          const emails = await res.json();
+          const emailDetail = emails.find((email) => email.verified == true);
+
+          if (!emailDetail) {
+            return done(new Error('no puedo obtener un correo electrónico válido para este usuario'));
+          }
+          profile.email = emailDetail.email;
+
+          let user = await UserModel.findOne({ email: profile.email });
+          if (!user) {
+            const newUser = {
+              email: profile.email,
+              firstName: profile._json.name || profile._json.login || 'noname',
+              lastName: 'nolast',
+              isAdmin: false,
+              password: 'nopass',
+              age: null,
+              cart: await cartService.createCart(),
+            };
+            let userCreated = await UserModel.create(newUser);
+            console.log('User Regristado Correctamente');
+            return done(null, userCreated);
+          } else {
+            console.log('User ya existe');
+            return done(null, user);
+          }
+        } catch (e) {
+          console.log('Error en autenticación github');
+          console.log(e);
+          return done(e);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    let user = await UserModel.findById(id);
+    done(null, user);
+  });
+}
